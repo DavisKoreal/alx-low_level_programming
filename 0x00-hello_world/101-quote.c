d190e96e17f8c295ddc253265d41059501e8521e
DavisKoreal <daviskipchirchir19@gmail.com>
2022-09-09 01:56:10 -0700

    extra

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Main entry point for the program
+ *
+ *   * @void: The function does not accept any parameters
+ *
+ *    * Return: int returned as 0
+ *
+ *     */
+
+int main(void)
+
+{
+           /*declaration*/
+	char quote = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
+
+
+           /*statements*/
+	putchar(quote);
+
+	return (1);
+
+}
