a3f7bd825ca9d84515520df11ab0144c23310a62
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-09-09 14:06:35 +0300

    rand

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,21 +2,16 @@
 
 
 /**
- *
- *  * main - Main entry point for the program
- *
+ *  *main - Main entry point for the program
  *   * @void: The function does not accept any parameters
- *
  *    * Return: int returned as 0
- *
  *     */
 
 int main(void)
 
 {
 
-	    puts("\"Programming is like building a multilingual puzzle");
-
-	    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
