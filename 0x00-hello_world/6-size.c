89bc2538a2d7917828b8ef6ec133ef7e4dec6e66
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-09-10 12:22:15 +0300

    chore 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,45 +1,31 @@
 #include <stdio.h>
 
 
-
 /**
- *
- *  * main - Main entry point for the program
- *
- *   * @void: The function does not accept any parameters
- *
- *    * Return: int returned as 0
- *
- *     */
+ *main - This is the main entry point for the program
+ *void - The function does not accept any parameters
+ *Return: int returned as 0
+ */
 
 int main(void)
 
 {
-          /*DECLARATIONS*/
 	int sizeChar, sizeInt, sizeLongInt, sizeLongLongInt, sizeFloat;
 
-          /*statements*/
+
 
 	sizeChar = sizeof(char);
-
 	sizeInt = sizeof(int);
-
 	sizeLongInt = sizeof(long int);
-
 	sizeLongLongInt = sizeof(long long int);
-
 	sizeFloat = sizeof(float);
 
 
 
 	printf("Size of a char: %d byte(s)\n", sizeChar);
-
 	printf("Size of an int: %d byte(s)\n", sizeInt);
-
 	printf("Size of a long int: %d byte(s)\n", sizeLongInt);
-
 	printf("Size of a long long int: %d byte(s)\n", sizeLongLongInt);
-
 	printf("Size of a float: %d byte(s)\n", sizeFloat);
 
 
