97111fb973c423ecaff57ce119b06fb3986b5345
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-09-10 11:30:01 +0300

    adding main definition

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 
-/*
- * @main - Main entry point for the program
- * @void: The function does not accept any parameters
+/**
+ * main: The entry point for the program
+ * void: The function does not accept any parameters
  * Return: int returned as 0
  */
 
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
