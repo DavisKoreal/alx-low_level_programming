ea0c6f36e64d8fde45551629aa0b430bc88951bf
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-09-10 12:54:08 +0300

    chore 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - Main entry point for the program
- *
- *   * @void: The function does not accept any parameters
- *
- *    * Return: int returned as 0
- *
- *     */
-
+ * main - Main entry point for the program
+ * void - The function does not accept any parameters
+ * Return: int returned as 0
+ */
 int main(void)
-
 {
-           /*declaration*/
-	char quote = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
+	char quote[] = 'and that piece of art is useful" - Dora Korpar, 2015-10-19';
 
-
-           /*statements*/
 	putchar(quote);
-
 	return (1);
-
 }
