6bbe1e4815f875154b961486d1411cca7d491a6d
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-09-10 11:20:07 +0300

    describing main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /*
- * main - Main entry point for the program
+ * @main - Main entry point for the program
  * @void: The function does not accept any parameters
  * Return: int returned as 0
  */
