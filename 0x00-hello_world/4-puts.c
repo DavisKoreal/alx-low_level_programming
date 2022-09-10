d2701e43c73e3deffa0b804f69875fed9b23273d
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-09-10 11:16:54 +0300

    adding betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 
-/**
- *  *main - Main entry point for the program
- *   * @void: The function does not accept any parameters
- *    * Return: int returned as 0
- *     */
+/*
+ * main - Main entry point for the program
+ * @void: The function does not accept any parameters
+ * Return: int returned as 0
+ */
 
 int main(void)
 
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
