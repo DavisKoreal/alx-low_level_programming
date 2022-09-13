5ed2ad0a0c73e60b696a980ec0283bf0abc8e071
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-09-13 03:45:44 +0300

    %T

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Main entry point for the program
- * @void: The function does not accept any parameters
- * Return: int returned as 0
+ * main - print quote
+ * Description: prints Dor's quote
+ * Return: Always 1 (success)
  */
+
 int main(void)
 {
-char quote[100] = {'a','n','d','t','h','a','t',' ','p','i','e','c','e','o','f', 'a','r','t', 'i','s','u','s','f','u,','l','"',' ','-','D','o','r','a',' ','K','o','r','p','a','r',',',' ','2','0','1','5','-','1','0','-','1','9','\0'};
-printf("%s",quote);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
