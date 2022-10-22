69182d54be6832f8964b9e9fed87307d66db2d53
Davis Kipchirchir <74677355+DavisKoreal@users.noreply.github.com>
2022-10-22 11:21:58 +0300

    flow

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
- * struct dog - struct used to hold information about a dog 
- * @name: name of the dog 
+ * struct dog - contains a struct for dog characteristics
+ * @name: name of the dog
  * @age: age of the dog
- * @owner: stores details of the dog's owner
+ * @owner: name of the dog's owner
  */
-
-struct dog 
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
